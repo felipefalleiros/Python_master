64be75cf077f168eca14bffa2f42537ccff2c0b0
felipefalleiros <mendesf84@gmail.com>
2026-02-17 16:49:50 -0300

    Aplica base template e campo de busca para filtrar consulta

--- cars/views.py
+++ cars/views.py
@@ -3,11 +3,11 @@ from cars.models import Car
 
 # Create your views here.
 def cars_view(request):
-    cars = Car.objects.all()
+    cars = Car.objects.all().order_by('-brand')
     search = request.GET.get('search')
     
     if search:
-        cars = cars.filter(model__contains=search)
+        cars = cars.filter(model__icontains=search)
     
     
     return render(
