36e188f56ce90072ae687c8b5b4715b3877bac21
felipefalleiros <mendesf84@gmail.com>
2026-02-17 16:13:15 -0300

    Filtra a busca baseado no parametro passado na url

--- cars/views.py
+++ cars/views.py
@@ -3,11 +3,13 @@ from cars.models import Car
 
 # Create your views here.
 def cars_view(request):
-    #cars = Car.objects.all() # Retorna todos os registros do
+    cars = Car.objects.all()
+    search = request.GET.get('search')
     
-    # cars = Car.objects.filter(brand__name='Fiat') # Retorna os registros onde a band é Fiat utilizando duplo underscore para pegar o nome na tabela que se vincula a Car
+    if search:
+        cars = cars.filter(model__contains=search)
     
-    cars = Car.objects.filter(model__contains='Fiat') # Retorna os registros que contém Fiat no nome do modelo (like 'nome%' no SQL)
+    
     return render(
         request,
         'cars.html',
