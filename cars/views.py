7758c53461e7af96405d3119e5c9d6238765529e
felipefalleiros <mendesf84@gmail.com>
2026-02-17 15:38:19 -0300

    Consultas com filtro na view

--- cars/views.py
+++ cars/views.py
@@ -3,8 +3,11 @@ from cars.models import Car
 
 # Create your views here.
 def cars_view(request):
-    cars = Car.objects.all()
+    #cars = Car.objects.all() # Retorna todos os registros do
     
+    # cars = Car.objects.filter(brand__name='Fiat') # Retorna os registros onde a band é Fiat utilizando duplo underscore para pegar o nome na tabela que se vincula a Car
+    
+    cars = Car.objects.filter(model__contains='Fiat') # Retorna os registros que contém Fiat no nome do modelo (like 'nome%' no SQL)
     return render(
         request,
         'cars.html',
